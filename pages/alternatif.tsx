e9fa102c2d85bbd100bf6d6d4608727634dc6207
usaidmasud <syaripmasud@gmail.com>
2022-06-09 10:15:10 +0800

    add alternatif page

--- pages/alternatif.tsx
+++ pages/alternatif.tsx
@@ -1,6 +1,7 @@
 import { NextPage } from "next";
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { Loading, PageHeader, RenderButtonAction } from "../components/atoms";
 import { Layout } from "../components/templates";
 import { fetchAlternatif } from "../utils/redux/slices/alternatifSlice";
 import { fetchCriteria } from "../utils/redux/slices/criteriaSlice";
@@ -16,67 +17,48 @@ const Alternatif: NextPage = () => {
     dispatch(fetchCriteria());
   }, [dispatch]);
 
-
   return (
     <Layout>
-      <div className="container mx-auto p-8">
-        <h1 className="text-2xl font-semibold text-gray-700">
-          Alternatif Pilihan
-        </h1>
-
+      <PageHeader title="Alternatif">
         {/*  START: Table */}
-        <div className="relative overflow-x-auto shadow-sm ">
-          <table className="table-auto w-full">
-            <thead className="text-sm text-gray-700 uppercase bg-gray-50">
-              <tr className="text-center">
-                <th rowSpan={2} scope="col" className="px-4 py-2">
+        <div className="table-responsive ">
+          <table className="table">
+            <thead>
+              <tr>
+                <th rowSpan={2} scope="col">
                   No
                 </th>
-                <th rowSpan={2} scope="col" className="px-4 py-2">
+                <th rowSpan={2} scope="col">
                   Alternatif
                 </th>
-                <th rowSpan={2} scope="col" className="px-4 py-2">
+                <th rowSpan={2} scope="col">
                   Project
                 </th>
-                <th colSpan={5} className="px-4 py-2">
-                  Value
-                </th>
-                <th rowSpan={2} scope="col" className="px-4 py-2">
+                <th colSpan={5}>Value</th>
+                <th rowSpan={2} scope="col">
                   Aksi
                 </th>
               </tr>
               <tr>
-                {
-                  stateCriteria.records.map((criteria, index) => (
-                    <th key={index} className="px-4 py-2">
-                      {criteria.code}
-                    </th>
-                  ))
-                }
+                {stateCriteria.records.map((criteria, index) => (
+                  <th key={index} className="px-4 py-2">
+                    {criteria.code}
+                  </th>
+                ))}
               </tr>
             </thead>
             <tbody>
+              {stateAlternatif.loading && <Loading colSpan={9} />}
               {stateAlternatif.records.map((alternatif, index) => (
                 <tr key={index}>
-                  <td className="border px-4 py-2">{index + 1}</td>
-                  <td className="border px-4 py-2">{alternatif.name}</td>
-                  <td className="border px-4 py-2">
-                    {alternatif.project.name}
-                  </td>
+                  <td>{index + 1}</td>
+                  <td>{alternatif.name}</td>
+                  <td>{alternatif.project.name}</td>
                   {alternatif.values.map((criteria, index2) => (
-                    <td key={index2} className="border px-4 py-2">
-                      {criteria.value}
-                    </td>
+                    <td key={index2}>{criteria.value}</td>
                   ))}
-                  <td className="border px-4 py-2">
-                    <div className="flex gap-1">
-                      <button className="bg-green-600 text-sm bg-opacity-80 hover:bg-opacity-100 text-white py-1 px-2 rounded">
-                        Edit
-                      </button>
-                      <button className="bg-red-600 text-sm bg-opacity-80 hover:bg-opacity-100 text-white py-1 px-2 rounded">
-                        Delete
-                      </button>
-                    </div>
+                  <td>
+                    <RenderButtonAction />
                   </td>
                 </tr>
               ))}
@@ -84,7 +66,7 @@ const Alternatif: NextPage = () => {
           </table>
         </div>
         {/*  END: Table */}
-      </div>
+      </PageHeader>
     </Layout>
   );
 };
