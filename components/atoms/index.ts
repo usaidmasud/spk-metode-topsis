aee04fa19f82af759a26b35d279a82788d1f7da7
usaidmasud <syaripmasud@gmail.com>
2022-06-09 10:14:41 +0800

    add export component atoms

---
+++ components/atoms/index.ts
@@ -0,0 +1,13 @@
+import Loading from "./Loading";
+import Menu from "./Menu";
+import PageHeader from "./PageHeader";
+import RenderBenefit from "./RenderBenefit";
+import RenderButtonAction from "./RenderButtonAction";
+export {
+  RenderBenefit,
+  RenderButtonAction,
+  PageHeader,
+  Loading,
+  Menu
+};
+
